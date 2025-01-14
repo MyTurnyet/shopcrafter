00946b3c18d173c02d024a3c21ca95796bb4c8a6
paige.watson <paige.watson@salesforce.com>
2025-01-14 15:56:47 -0800

    equate items by price

--- src/main/java/com/softwareascraft/shopcrafter/cart/Item.java
+++ src/main/java/com/softwareascraft/shopcrafter/cart/Item.java
@@ -3,13 +3,17 @@ package com.softwareascraft.shopcrafter.cart;
 public class Item implements Equatable<Item> {
     private final int sku;
     private final String name;
+    private final int price;
 
-    public Item(int sku, String name) {
+    public Item(int sku, String name, int price) {
         this.sku = sku;
         this.name = name;
+        this.price = price;
     }
 
     public boolean isSameAs(Item otherItem) {
-        return this.sku == otherItem.sku && this.name.equals(otherItem.name);
+        return this.sku == otherItem.sku
+               && this.name.equals(otherItem.name)
+                && this.price == otherItem.price;
     }
 }
--- src/test/java/com/softwareascraft/shopcrafter/cart/ItemTests.java
+++ src/test/java/com/softwareascraft/shopcrafter/cart/ItemTests.java
@@ -9,24 +9,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ItemTests {
     @Test
     void itemMatchesSkuAndName() {
-        Item item1 = new Item(1234, "Item Name");
-        Item item2 = new Item(1234, "Item Name");
+        Item item1 = new Item(1234, "Item Name", 0);
+        Item item2 = new Item(1234, "Item Name", 0);
         boolean matches = item1.isSameAs(item2);
         assertThat(matches).isTrue();
+    }
+    @Test
+    void doesNotMatchWithDifferentPrice() {
+        Item item1 = new Item(1234, "Item Name", 123);
+        Item item2 = new Item(1234, "Item Name",10);
+        boolean matches = item1.isSameAs(item2);
+        assertThat(matches).isFalse();
     }
 
     @Test
     void doesNotMatchWithDifferentSKU() {
-        Item item1 = new Item(1234, "Item Name");
-        Item item2 = new Item(2345, "Item Name");
+        Item item1 = new Item(1234, "Item Name", 0);
+        Item item2 = new Item(2345, "Item Name", 0);
         boolean matches = item1.isSameAs(item2);
         assertThat(matches).isFalse();
     }
+
     @Test
     void doesNotMatchWithDifferentNames() {
-        Item item1 = new Item(1234, "Item Name");
-        Item item2 = new Item(1234, "Other Name");
+        Item item1 = new Item(1234, "Item Name", 0);
+        Item item2 = new Item(1234, "Other Name", 0);
         boolean matches = item1.isSameAs(item2);
         assertThat(matches).isFalse();
     }
+
 }

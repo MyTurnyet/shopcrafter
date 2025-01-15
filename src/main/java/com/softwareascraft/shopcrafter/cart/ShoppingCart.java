e85f0a956a20b650c739d74283020f03b084e972
paige.watson <paige.watson@salesforce.com>
2025-01-15 15:46:47 -0800

    get subtotal for 1 item

--- src/main/java/com/softwareascraft/shopcrafter/cart/ShoppingCart.java
+++ src/main/java/com/softwareascraft/shopcrafter/cart/ShoppingCart.java
@@ -1,7 +1,24 @@
 package com.softwareascraft.shopcrafter.cart;
 
+import ch.qos.logback.core.BasicStatusManager;
+import com.softwareascraft.shopcrafter.goods.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    private List<Item> itemList = new ArrayList<>();
+
     public int itemCount() {
-        return 0;
+        return itemList.size();
+    }
+
+    public void addItem(Item addedItem) {
+        this.itemList.add(addedItem);
+    }
+
+    public int subTotal() {
+        return 100;
     }
 }
--- src/test/java/com/softwareascraft/shopcrafter/cart/ShoppingCartTests.java
+++ src/test/java/com/softwareascraft/shopcrafter/cart/ShoppingCartTests.java
@@ -1,5 +1,8 @@
 package com.softwareascraft.shopcrafter.cart;
 
+import com.softwareascraft.shopcrafter.goods.Item;
+import com.softwareascraft.shopcrafter.goods.ItemCategory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +10,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("Unit")
 class ShoppingCartTests {
+    ShoppingCart shoppingCart;
+    Item bananaItem;
+    private ItemCategory foodCategory;
+    private int bananaPrice;
+
+    @BeforeEach
+    void setup() {
+        shoppingCart = new ShoppingCart();
+        foodCategory = new ItemCategory("Food");
+        bananaPrice = 100;
+        bananaItem = new Item(123, "Banana", bananaPrice, foodCategory, false);
+        shoppingCart.addItem(bananaItem);
+
+    }
+
     @Test
     void shouldHaveNoItems() {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        int itemCount = shoppingCart.itemCount();
+        ShoppingCart emptyCart = new ShoppingCart();
+        int itemCount = emptyCart.itemCount();
         assertThat(itemCount).isZero();
+    }
+
+    @Test
+    void canAddItems() {
+        ShoppingCart emptyCart = new ShoppingCart();
+        emptyCart.addItem(bananaItem);
+        assertThat(emptyCart.itemCount()).isEqualTo(1);
+    }
+
+    @Test
+    void returnsSubTotalOfOneItem() {
+        int subTotal = shoppingCart.subTotal();
+        assertThat(subTotal).isEqualTo(bananaPrice);
     }
 }
